91dce8ce03e4afc46fac3f0796891eec5ec03347
Inyoung <eyewhy92@gmail.com>
2023-08-06 23:55:51 +0900

    수정사항 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, {useState, useCallback} from 'react';
 import './App.css'
 
+let renderCount = 0;
+
 function App() {
+
+    //use state
     const [number, setNumber] = useState(0);
-  const [clickNumber, setClickNumber] = useState(0);
-
-//useCallback
-//1. 함수를 캐싱( useCallback을 사용하여 incrementNumber 함수를 캐싱 )
-
-//2. 훅 기능 : 리렌더링 시, 이 전 생성된 함수를 재사용할 수 있도록 도와줌
+    //const [render, setRender] = useState(1);
 
 
-  const incrementNumber = useCallback(() => {
-    setNumber(prevCount => prevCount + 1);
-    setClickNumber(clickNumber + 1);
-  }, [clickNumber]);
+    //use callback
+    //useCallback hook이 number를 업데이트 시키는 새로운 함수를 만드는데 사용
+    //리액트가 setNumber가 변하더라도 매번 렌더링되는걸 방지. 메모이제이션(캐쉬)된 함수 실행
 
-  // useMemo를 사용하여 number 값을 저장, 매 클릭 시마다 리렌더링을 방지.
-  const memoization = useMemo(() => {
-    return number;
-  }, [number]);
+    const countNum = useCallback(() => {
+        setNumber(numCount => numCount + 1);
+    }, []); //처음 컴포넌트가 실행 시만?
 
+    console.log("countNum" + countNum);
+    /*
+        const countNum = setNumber(prevCount => prevCount + 1);
+        console.log("countNumTwo" + countNumTwo)*/
+    renderCount ++;
+    console.log('render ' + renderCount);
 
   return (
     <>
       <div>
         <span>첫번째 클릭(하나씩 증가)</span>
-        <button onClick={incrementNumber}>
-          {memoization}번 클릭 했습니다!
+        <button onClick={()=>{
+          setNumber((prev)=> {return prev +1})
+        }}>
+          {number}번 클릭 했습니다!
         </button>
       </div>
 
       <div>
         <span>두번째 클릭(첫번째 클릭 수 만큼 더해서 증가)</span>
-        <button onClick={() => setClickNumber(clickNumber + number)}>
-          {clickNumber}번 클릭 했습니다! 
-        </button>
+        {number * 2}번 클릭 했습니다!
       </div>
     </>
   )
